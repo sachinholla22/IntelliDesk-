c3101c71d8dcc376c46f6810d651a0abaf35b600
sachin <sachinholla01@gmail.com>
2025-08-22 17:52:47 +0530

    Plan Controller added

---
+++ ticketsystem/src/main/java/com/ticketsystem/ticketsystem/controller/PlanController.java
@@ -0,0 +1,10 @@
+package com.ticketsystem.ticketsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/upgrade")
+public class PlanController {
+    
+}
